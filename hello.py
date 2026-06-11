ca1140f9db95e7bd39cb5626edfc4b07a42554ed
shabnamsardar609-spec <shabnamsardar609@gmail.com>
2026-06-12 01:57:56 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,37 @@
 print("Hello GitHub!")
+
+name="shabnam"
+age=20
+price=68.99
+
+print("my name is:",name) #my name is:shabnam
+print("my age is:",age) #my age is:20
+print("my price is:",price) #my price is:68.99
+
+print(type(name)) #str
+print(type(age)) #int
+print(type(price)) #float
+
+age=20
+old=True
+a=None
+print(type(old)) #bool
+print(type(a)) #Nonetype
+
+#arithmatic operators
+a=4
+b=2
+sum=a+b
+print(sum) #6
+print(a+b) #6
+print(a-b) #2
+print(a*b) #8
+print(a/b) #2.0
+print(a%b) #rem 0
+print(a**b) #16
+print(a==b) #False
+print(a!=b) #True
+print(a>=b) #True
+print(a>b) #True
+print(a<=b) #False
+print(a<b) #False
